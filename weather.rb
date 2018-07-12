d0eea4c1dd53a34c464caba1101c69eb0d2a765d
meganletendre <megan.letendre@comcast.net>
2018-07-12 15:03:56 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -18,7 +18,7 @@ end
 
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal. 
 
-def current_weather(weather)
+def weather_report(weather)
   city = weather[:city]
   description = weather[:description]
   temperature = weather[:temperature_farenheit]
